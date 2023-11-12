7c3be9e111566a9682c4a5ae92b89813839d8461
Geolek <gxlekkas@gmail.com>
2023-11-12 02:59:31 +0200

    Data preprocessing

---
+++ Data_preprocessing.py
@@ -0,0 +1,15 @@
+# Import necessary packages
+
+import pandas as pd
+import numpy as np
+import nltk
+from nltk.tokenize import word_tokenize  # Tokenization (split texts into individual word or tokens)
+from nltk.corpus import stopwords  # Remove stopwords (e.g., 'the','is')
+import string  # Handle strings
+import re  # Regular expressions - string manipulation
+
+
+# Ensure NLTK components are downloaded
+
+nltk.download('punkt')
+nltk.download('stopwords')
